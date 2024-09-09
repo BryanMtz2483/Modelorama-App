47a4d2bbd1a721c4fb22c36cac3df9da02c08daa
Bryan <mhzb3072@gmail.com>
2024-09-08 19:14:18 -0600

    Correción de errores y agregadas vistas faltantes

--- app/Http/Controllers/Crear_Producto.php
+++ app/Http/Controllers/Crear_Producto.php
@@ -31,6 +31,29 @@ class Crear_Producto extends Controller
 
         return redirect()->route('producto.index');
     }
+    public function update($product){
+        $lastProduct = Product::find($product);
+        return view('product.update-producto',compact('lastProduct'));
+    }
+    public function change(Request $request, $id){
+        $product = Product::findOrFail($id);
+
+        $product->name = $request->name;
+        $product->branch = $request->branch;
+        $product->product_number = $request->product_number;
+        $product->price = $request->price;
+        $product->desc = $request->desc;
+        $product->save();
+        return redirect()-> route('producto.show', $product)->with('edited','Producto Actualizado');
+    }
+    public function delete($product){
+        
+        $eliminar = Product::findOrFail($product);
+
+        $eliminar -> delete();
+        
+        return redirect()-> route('producto.index')->with('success','Producto Borrado');
+    }
     public function getReport(){
         $products = Product::all();
         $pdf = Pdf::loadView('product.report-product', compact('products'));
--- resources/views/product/show-producto.blade.php
+++ resources/views/product/show-producto.blade.php
@@ -1,20 +1,5 @@
 <x-app-layout>
 
-        <!--<p class="text-6xl tex-blue-900 dark:text-white">Numero de producto: </p>
-        <p class="text-4xl font-thin text-gray-900 dark:text-white">{{$productDetail->product_number}}</p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-
-        <p class="text-6xl text-blue-900 dark:text-white">Nombre: </p>
-        <p class="text-4xl font-thin text-gray-900 dark:text-white">{{$productDetail->name}}</p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-    
-        <p class="text-6xl text-blue-900 dark:text-white">Descripcion: </p>
-        <p class="text-4xl font-thin text-gray-900 dark:text-white">{{$productDetail->desc}}</p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-    
-        <p class="text-6xl text-blue-900 dark:text-white">Precio: </p>
-        <p class="text-4xl font-thin text-gray-900 dark:text-white">{{$productDetail->price}}</p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">-->
         <style>
             img{
                 width: 90%;
